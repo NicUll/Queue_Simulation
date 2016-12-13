cddfb1c37329a66558aacbf9cd103fee88ae93e1
NicUll <lordtardus@gmail.com>
2016-12-13 19:47:10 +0100

    Fixed event handler

--- app/model.py
+++ app/model.py
@@ -4,13 +4,13 @@ from random import random
 
 
 class Model(object):
-    def __init__(self, times, time_per_customer, frequency, x_size="800", y_size="640"):
+    def __init__(self, times, time_per_customer, odds, x_size="800", y_size="640"):
         """Model object that holds the simulation parameters
         and an office object."""
         self.office = Office(times, time_per_customer)  # Create and store a post-office
         self.next_out_time = None  # Initialize a field that will keep track of current customers out time
         self.customers_in_queue = 0  # Total amount of customers that's been handled
-        self.frequency = frequency  # The frequency with which customers enter the office
+        self.odds = odds  # The odds of a customer entering the office
         self.current_timestring = times[0]  # The current time in text format, e.g. 10:58
         self.x_size = x_size
         self.y_size = y_size
@@ -21,31 +21,33 @@ class Model(object):
         i.e. one minute."""
 
         self.event_handler.time = self.current_timestring  # Update the event handlers
-        self.event_handler.clear_events()  # time and erase prev events
+        self.event_handler.clear_events()                  # time and erase prev events
+        self.customers_in_queue = len(self.office.customers)
+        queue_not_empty_before = self.customers_in_queue and True
 
         # Block that handles generating new customers
         customer = None  # Generate a customer with a change of "frequency" e.g. 0.2
-        if (random() < self.frequency) and self.office.open:
+        if (random() < self.odds) and self.office.open:
             customer = self.office.add_customer()
             self.event_handler.add_event("Kund {} kommer in".format(customer.id))
 
+            if queue_not_empty_before:
+                self.event_handler.add_event("får plats {} i kön".format(self.customers_in_queue + 1), increase=False)
+
         # Block that handles finishing current customer
-        queue_empty_before = True
         if self.next_out_time == self.office.clock:  # Check if a customer should be done now and
-            self.event_handler.add_event("får plats {} i kön".format(len(self.office.customers)), increase=False)
             prev_customer = self.office.finish_customer()  # Remove them from the queue
             self.event_handler.add_event("Kund {} går".format(prev_customer.id))
             self.next_out_time = None
-            queue_empty_before = False
 
         # Block that handles getting to the next customer
-        self.customers_in_queue = len(self.office.customers)
+        self.customers_in_queue = len(self.office.customers)  # Update length of queue
         # Check if no customer is being helped and there is a queue
         if self.next_out_time is None and self.customers_in_queue > 0:
             self.next_out_time = self.office.handle_customer()
             next_customer = self.office.customers[0]
             event_string = "kund {} blir betjänad".format(next_customer.id)
-            if (next_customer == customer) and queue_empty_before:
+            if (next_customer == customer) and not queue_not_empty_before:
                 event_string = "blir genast betjänad"
             self.event_handler.add_event(event_string, increase=False)
 
